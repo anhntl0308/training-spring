1484feb1c091c8fe45f2b930eb6b54aca9688f5d
= <giakinh2000@gmail.com>
2022-12-30 14:34:35 +0700

    controller advice

--- src/main/java/com/example/demomapstruct/advice/GlobalAdvice.java
+++ src/main/java/com/example/demomapstruct/advice/GlobalAdvice.java
@@ -1,12 +1,12 @@
 package com.example.demomapstruct.advice;
 
-import com.example.demomapstruct.exception.ApiError;
 import com.example.demomapstruct.payload.BaseResponse;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.validation.ConstraintViolationException;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalAdvice extends ResponseEntityExceptionHandler {
@@ -35,4 +36,16 @@ public class GlobalAdvice extends ResponseEntityExceptionHandler {
 //                                                                      WebRequest request) {
 //        return ResponseEntity.badRequest().body(BaseResponse.builder().data(ex.getMessage()).message("Bad Request").build());
 //    }
+
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                               HttpHeaders headers,
+                                                               HttpStatus status,
+                                                               WebRequest request) {
+        return ResponseEntity.badRequest().body(BaseResponse.builder().data(getErrorMessageFromException(ex)).message("Bad Request").build());
+    }
+
+    private String getErrorMessageFromException(MethodArgumentNotValidException ex) {
+        return ex.getFieldErrors().stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(","));
+    }
 }
